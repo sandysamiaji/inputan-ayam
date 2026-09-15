1e9661b253d8b2b7a247555382182b9e5aa01a7c
sandysamiaji <sandysamiaji0125@gmail.com>
2026-09-16 01:29:06 +0700

    ngerubah

--- resources/views/input/index.blade.php
+++ resources/views/input/index.blade.php
@@ -672,14 +672,16 @@
                         <select name="coop_id" id="pakanCoop" onchange="updateCoopPop('pakan')" required>
                             @foreach($coops as $coop)
                                 @php
-                                    $cStd = $coopStandards[$coop->id] ?? ['feed_target_gram' => 105, 'tips' => 'Layer'];
+                                    $cStd = $coopStandards[$coop->id] ?? [];
+                                    $feedTargetGram = $cStd['feed_target_gram'] ?? $cStd['gram_pakan'] ?? 105;
+                                    $tips = $cStd['tips'] ?? 'Layer';
                                 @endphp
                                 <option value="{{ $coop->id }}" 
                                         data-flock="{{ $coop->flock_id }}" 
                                         data-pop="{{ $coop->active_chickens }}"
                                         data-age="{{ $coop->chicken_age_weeks }}"
-                                        data-feed="{{ $cStd['feed_target_gram'] }}"
-                                        data-feedtype="{{ $cStd['tips'] }}">
+                                        data-feed="{{ $feedTargetGram }}"
+                                        data-feedtype="{{ $tips }}">
                                     {{ $coop->name }}
                                 </option>
                             @endforeach

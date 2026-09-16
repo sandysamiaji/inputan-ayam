56821e903ae5655f3d1adafc09d14d95afa7e1a6
sandysamiaji <sandysamiaji0125@gmail.com>
2026-09-16 23:44:46 +0700

    lupa

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -35,4 +35,9 @@ class Sale extends Model
     {
         return $this->hasMany(SaleItem::class);
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
